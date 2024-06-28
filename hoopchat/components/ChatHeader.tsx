8fa8ba296230466af021d0659e68939794e5a884
rahulsainlll <rahulsain.social777@gmail.com>
2024-06-28 11:11:47 +0530

    email-login-done

--- hoopchat/components/ChatHeader.tsx
+++ hoopchat/components/ChatHeader.tsx
@@ -1,7 +1,17 @@
-import { Button } from "./ui/button";
 import React from "react";
+import { Button } from "./ui/button";
+import { signOut } from "@/app/login/actions";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 
-export default function ChatHeader() {
+export default async function ChatHeader() {
+
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <div className="h-20">
       <div className="p-5 border-b flex justify-between items-center">
@@ -12,7 +22,18 @@ export default function ChatHeader() {
             <h1 className="text-sm text-gray-400">2 onlines</h1>
           </div>
         </div>
-        <Button variant="outline">Login</Button>
+        <div className="flex flex-1 items-center justify-end space-x-2">
+          {user !== null ? (
+            <form action={signOut} className="flex items-center gap-2">
+              <p>{user.email}</p>
+              <Button variant="outline">Sign Out</Button>
+            </form>
+          ) : (
+            <Button variant="outline" asChild>
+              <Link href="/login">Sign In</Link>
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
