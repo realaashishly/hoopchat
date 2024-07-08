d11dc321ce661fbf929a4ee58f3e921bcf942647
Aashish Singh <11aashish.singh@gmail.com>
2024-07-08 09:49:37 +0530

    fix map key issue

--- hoopchat/app/login/oauth-signin.tsx
+++ hoopchat/app/login/oauth-signin.tsx
@@ -5,34 +5,35 @@ import { Github } from "lucide-react";
 import { oAuthSignIn } from "./actions";
 
 type OAuthProvider = {
-  name: Provider;
-  displayName: string;
-  icon?: JSX.Element;
+    name: Provider;
+    displayName: string;
+    icon?: JSX.Element;
 };
 
 export function OAuthButtons() {
-  const oAuthProviders: OAuthProvider[] = [
-    {
-      name: "github",
-      displayName: "GitHub",
-      icon: <Github className="size-5" />,
-    },
-  ];
+    const oAuthProviders: OAuthProvider[] = [
+        {
+            name: "github",
+            displayName: "GitHub",
+            icon: <Github className='size-5' />,
+        },
+    ];
 
-  return (
-    <>
-      {oAuthProviders.map((provider) => (
-        <Button
-          className="w-full flex items-center justify-center gap-2"
-          variant="outline"
-          onClick={async () => {
-            await oAuthSignIn(provider.name);
-          }}
-        >
-          {provider.icon}
-          Login with {provider.displayName}
-        </Button>
-      ))}
-    </>
-  );
+    return (
+        <>
+            {oAuthProviders.map((provider, index) => (
+                <Button
+                    key={index}
+                    className='w-full flex items-center justify-center gap-2'
+                    variant='outline'
+                    onClick={async () => {
+                        await oAuthSignIn(provider.name);
+                    }}
+                >
+                    {provider.icon}
+                    Login with {provider.displayName}
+                </Button>
+            ))}
+        </>
+    );
 }
